6c78b01f185151f2f92446973721f4baeac122a5
Student aapna collage mine <student@gmail.com>
2025-06-02 12:49:16 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-800 text-center underline">my text sample</h1>
+    </div>
+  )
+}
+
+export default App;
